db56623b022b69814e16ea20005bcdd4b29dbeea
Hope <hope.aitchison@ljnmc2k795.home>
2024-04-19 15:47:59 +0100

    removing testing infra and adding a readme

--- app/main.tf
+++ app/main.tf
@@ -11,18 +11,15 @@ module "server-sg" {
 
   # default CIDR block, used for all ingress rules - typically CIDR blocks of the VPC
   ingress_cidr_blocks = [data.aws_vpc.dev.cidr_block]
-  ingress_rules       = ["https-443-tcp"]
-
-  ingress_with_cidr_blocks = [
+  ingress_with_source_security_group_id = [
     {
-      rule        = "ssh-tcp"
-      cidr_blocks = local.ip_secret
-    },
+      rule = "https-443-tcp"
+      source_security_group_id = data.aws_security_group.vpc-endpoints-sg.id
+    }
   ]
 
   egress_cidr_blocks = [data.aws_vpc.dev.cidr_block]
   egress_rules       = ["https-443-tcp"]
-
   egress_with_cidr_blocks = [
     {
       rule        = "https-443-tcp"
@@ -42,33 +39,6 @@ module "ec2_instance" {
   monitoring             = true
   vpc_security_group_ids = [module.server-sg.security_group_id]
   subnet_id              = data.aws_subnet.private.id
-
-  create_iam_instance_profile = true
-  iam_role_description        = "IAM role for Redhat EC2"
-  iam_role_policies = {
-    AmazonEC2RoleforSSM = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  }
-
-  user_data_base64            = filebase64("user_data.sh")
-  user_data_replace_on_change = true
-
-  tags = {
-    Environment = var.stage
-  }
-}
-
-module "ec2_instance_public" {
-  source = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "redhat-server-public"
-
-  ami                    = data.aws_ami.rhel_9.id
-  instance_type          = "t2.micro"
-  key_name               = var.key-pair
-  monitoring             = true
-  vpc_security_group_ids = [module.server-sg.security_group_id]
-  subnet_id              = data.aws_subnet.public.id
-  # associate_public_ip_address = true
 
   create_iam_instance_profile = true
   iam_role_description        = "IAM role for Redhat EC2"
